43d1b534ba29d3ce26584a0b93b58ead3cd204a3
Jaden Mack <omegajaden@gmail.com>
2022-04-20 14:02:19 -0400

    Added docstrings, separated children and adults, combined name functions

--- projecttest.py
+++ projecttest.py
@@ -177,6 +177,7 @@ class Child:
     child_list = list(child_list)
 
     def __init__(self, house):
+        """initializes child"""
         self.gender = None
         self.name = None  # first name will be randomized, last name will be the household name, blank until generated later
         self.house = house  # last name/house they live in
@@ -187,6 +188,7 @@ class Child:
         Child.child_list += [(self.full_name, self.gender, self.age)]
 
     def assign_gender(self):
+        """grants a random binary gender"""
         gender_seed = random.randrange(0, 2, 1)
         if gender_seed == 0:
             self.gender = "female"
@@ -194,26 +196,20 @@ class Child:
             self.gender = "male"
 
     def give_name(self, gender):
+        """chooses list to run name function with, assigns name"""
         if gender == "male":
-            self.name = self.random_male_name(
+            self.name = self.random_name(
                 r"C:\Users\jaden\OneDrive\Documents\Northeastern\Northeastern\2022 Spring\Computing Fundamentals\final project\malenames.txt")
         if gender == "female":
-            self.name = self.random_female_name(
+            self.name = self.random_name(
                 r"C:\Users\jaden\OneDrive\Documents\Northeastern\Northeastern\2022 Spring\Computing Fundamentals\final project\femalenames.txt")
 
-    def random_male_name(self, names):
+    def random_name(self, names):
+        """separates external .txt into a list, gets a random name, assigns it"""
         f = open(names)
         name_list = f.read()
         name_list_parced = name_list.split()
-        seed = random.randrange(0, 2942, 1)
-        self.name = name_list_parced[seed]
-        return (self.name)
-
-    def random_female_name(self, names):
-        f = open(names)
-        name_list = f.read()
-        name_list_parced = name_list.split()
-        seed = random.randrange(0, 5003, 1)
+        seed = random.randrange(0, len(name_list_parced) - 1, 1)
         self.name = name_list_parced[seed]
         return (self.name)
 
